194cc8b64aa6220d10720fabf3a8edeb8d6b40d3
blakeCS <35789943+blakeCS@users.noreply.github.com>
2018-10-10 18:17:35 -0500

    Update OutcomesData.js

--- OutcomesData.js
+++ OutcomesData.js
@@ -48,7 +48,7 @@ var collectData = function() {
       window.data[i].data[j] = float(scores[j].innerHTML);
     }
     
-    htmlString += '<canvas id="chart' + i + '" width="800" height="400"></canvas>'
+    htmlString += '<canvas id="chart' + i + '" width="800" height="400"></canvas>';
 
     htmlString += '</div>';
   }
