0c6fe0bbb9e7779f972ea7252ae725a6b092b1f0
blakeCS <35789943+blakeCS@users.noreply.github.com>
2018-10-11 16:45:42 -0500

    Update OutcomesData.js

--- OutcomesData.js
+++ OutcomesData.js
@@ -141,7 +141,7 @@ var collectData = function() {
       htmlString += '<div style = "float: left; width: 43.5%; padding: 10px 1.5%; margin: 15px 1.5%; border: 1px solid #000; border-radius: 8px; min-height: 370px; page-break-inside: avoid;">'
       htmlString += '<h3>' + window.data[i].name + '</h3>';
       htmlString += '<canvas id="chart' + i + '" style="width:90%;height:200px;"></canvas>';
-      htmlString += '<h2> Outcome Average: ' + window.data[i].average + '</h2>';
+      htmlString += '<h2> Outcome Average:  ' + window.data[i].average + '</h2>';
       htmlString += '</div>';
   }
   
